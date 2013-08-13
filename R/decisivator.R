e45b45fc1f3885998c7804367a6b5ff183ef5f89
izhbannikov <i.zhbannikov@gmail.com>
2013-08-13 04:11:23 -0700

    Minor changes

--- R/decisivator.R
+++ R/decisivator.R
@@ -1,7 +1,8 @@
 isdecisive <- function(filename, unrooted=T, fflag=F,format="csv") {
   
   if(format == "nexus") {
-    print(paste("Reading input file ",filename,sep=''))
+    #print(paste("Reading input file ",filename,sep=''))
+    print("Reading input file...")
     data <- read_nexus(filename)
     #======Taxa==========
     X <- rownames(data)
@@ -10,11 +11,14 @@ isdecisive <- function(filename, unrooted=T, fflag=F,format="csv") {
     S <- lapply( seq_len(length(S)), function(i) lapply( seq_len(length(S[[i]])), function(j) if ( S[[i]][j] == 1 ) { S[[i]][j] <- X[j] } else { S[[i]][j] <- "NA"  } ) )
     S <- lapply( seq_len(length(S)), function(i) S[[i]] <- S[[i]][which(S[[i]] != "NA")]  )
     S <- lapply( seq_len(length(S)), function(i) unlist(S[[i]]))
-    print(paste("Done reading input file ",filename,sep=''))
+    #print(paste("Done reading input file ",filename,sep=''))
+    print("Done reading input file.")
   }else if(format == "csv") {
-    print(paste("Reading input file ",filename,sep=''))
+    #print(paste("Reading input file ",filename,sep=''))
+    print("Reading input file...")
     #================Loading data==========================#
     data <- read.csv(filename,header=T)
+    colnames(data)[1] <-"T"
     #======Taxa==========
     X <- as.character(lapply(seq_len(dim(data)[1]), function(i) toString(data[i,1])))
     #======Partitions of data set======================
@@ -23,7 +27,8 @@ isdecisive <- function(filename, unrooted=T, fflag=F,format="csv") {
     S <- lapply( seq_len(length(S)), function(i) lapply( seq_len(length(S[[i]])), function(j) if ( S[[i]][j] == 1 ) { S[[i]][j] <- X[j] } else { S[[i]][j] <- "NA"  } ) )
     S <- lapply( seq_len(length(S)), function(i) S[[i]] <- S[[i]][which(S[[i]] != "NA")]  )
     S <- lapply( seq_len(length(S)), function(i) unlist(S[[i]]))
-    print(paste("Done reading input file ",filename,sep=''))
+    #print(paste("Done reading input file ",filename,sep=''))
+    print("Done reading input file.")
   } else {
     print(paste("Unknown format option: ",format,sep=''))
     return
@@ -51,20 +56,25 @@ isdecisive <- function(filename, unrooted=T, fflag=F,format="csv") {
   for(i in seq_len(length(ans)-1)) {
     for(j in seq_len(length(ans[[i]]))) {final[ ans[[i]][j], colnames(final)[i] ] <- 1}
   }
-  #Add a '*' and compute the list of suggested characters in form of {taxon, character}:
-  suggested <- c()
-  for(i in 1:dim(final)[1]) {
-  	for(j in 1:dim(final)[2]) {
-  		if(final[i,j] != data[i,j+1]) {
-        final[i,j] = '*'
-        pair <- c(rownames(final)[i],colnames(final)[j])
-        suggested <-rbind(suggested, pair)
-  		}
-  	}
+  
+  if(fflag == TRUE) {
+    #Add a '*' and compute the list of suggested characters in form of {taxon, character}:
+    suggested <- c()
+    for(i in 1:dim(final)[1]) {
+  	  for(j in 1:dim(final)[2]) {
+  		  if(final[i,j] != data[i,j+1]) {
+          final[i,j] = '*'
+          pair <- c(rownames(final)[i],colnames(final)[j])
+          suggested <-rbind(suggested, pair)
+  		  }
+  	  }
+    }
+    colnames(suggested) <- c("T","C")
+    rownames(suggested) <- seq_len(dim(suggested)[1])
+    out <- list(final,ans[[length(ans)]],suggested)
+  } else {
+    out <- list(final,ans[[length(ans)]])
   }
-  colnames(suggested) <- c("T","C")
-  rownames(suggested) <- seq_len(dim(suggested)[1])
-  out <- list(final,ans[[length(ans)]],suggested)
 }
 
 read_nexus <- function(filename) {
